b9117a2e68e2f05481aa4be509ca77ecc252c876
migaa10 <migaraten@gmail.com>
2023-05-24 22:51:22 +0530

    add userfind query

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -1,20 +1,20 @@
 const authModel = require('../models/userModel') // import authModel
 const findUser = async (userData) => {
-    try {
-        const query = { userName: userData.userName };
-        const data = await authModel.find(query).maxTimeMS(10000); // Increased timeout to 30 seconds
-        return data;
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal server error' });
-        return null;
-    }
+    const query = { userName: userData.userName };
+    const data = await authModel.find(query).maxTimeMS(90000); // Increased timeout to 30 seconds
+    return data;
 
 };
 
 const authRegister = async (req, res) => {
-    const data = await findUser(req.body);
-    res.send({ category: data });
+    try {
+        const data = await findUser(req.body);
+        res.send({ category: data });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Internal server error' });
+    }
 };
 
 module.exports = {
