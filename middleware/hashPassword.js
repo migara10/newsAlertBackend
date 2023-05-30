fdd3e4d5fe3ffd6f4d0f10b0b166ffea7d1c71e3
migaa10 <migaraten@gmail.com>
2023-05-30 20:15:48 +0530

    add user login function

--- middleware/hashPassword.js
+++ middleware/hashPassword.js
@@ -1,3 +1,4 @@
+const authModel = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 
 const encryptPassword = async (password) => {
@@ -10,6 +11,30 @@ const encryptPassword = async (password) => {
   }
 };
 
+const findUser = async (userData) => {
+  try {
+    const query = { userName: userData.userName };
+    const data = await authModel.findOne(query);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const decriptPassword = async (password, hash, callback) => {
+  bcrypt.compare(password, hash, (err, res) => {
+    if (err) throw err
+    if (res) {
+      callback(null, res)
+    }
+    if (!res) {
+      callback(null)
+    }
+  });
+}
+
 module.exports = {
   encryptPassword,
+  findUser,
+  decriptPassword,
 };
