c50800e2e1b9704b0f2f70de6fc7a847d1e233f2
migaa10 <migaraten@gmail.com>
2023-05-31 20:22:31 +0530

    add refreshtoken and access token functions

--- middleware/hashPassword.js
+++ middleware/hashPassword.js
@@ -1,5 +1,7 @@
 const authModel = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); // config env
 
 const encryptPassword = async (password) => {
   try {
@@ -33,8 +35,23 @@ const decriptPassword = async (password, hash, callback) => {
   });
 }
 
+const verifyToken = (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('bearer')) {
+    const token = req.headers.authorization.split(' ')[1];
+    if (token == null) res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+      if (err) res.sendStatus(403);
+      req.user = user;
+      next();
+    })
+  } else {
+    res.sendStatus(401);
+  }
+}
+
 module.exports = {
   encryptPassword,
   findUser,
   decriptPassword,
+  verifyToken,
 };
